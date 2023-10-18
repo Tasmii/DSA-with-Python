4eb0c96da4b04f6b805e82010e67725539983fa2
Tasmiya Ilahi <91000777+Tasmii@users.noreply.github.com>
2023-10-18 23:42:57 +0530

    Create Traversal.py

---
+++ Binary Search Tree/Traversal.py
@@ -0,0 +1,48 @@
+#Time and Space complexity is O(1)
+class BSTNode:
+    def __init__(self, data):
+        self.data = data
+        self.leftChild = None
+        self.rightChild = None
+
+#Time and Space complexity is O(logN)
+def insertNode(rootNode, nodeValue):
+    if rootNode.data == None:
+        rootNode.data = nodeValue
+    elif nodeValue <= rootNode.data:
+        if rootNode.leftChild is None:
+            rootNode.leftChild = BSTNode(nodeValue)
+        else:
+            insertNode(rootNode.leftChild, nodeValue)
+    else:
+        if rootNode.rightChild is None:
+            rootNode.rightChild = BSTNode(nodeValue)
+        else:
+            insertNode(rootNode.rightChild, nodeValue)
+    return "The node has been successfully inserted"
+
+#Depth First Search
+
+#Time and Space complexity is O(N)
+def preOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    print(rootNode.data)
+    preOrderTraversal(rootNode.leftChild)
+    preOrderTraversal(rootNode.rightChild)
+
+#Time and Space complexity is O(N)
+def inOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    inOrderTraversal(rootNode.leftChild)
+    print(rootNode.data)
+    inOrderTraversal(rootNode.rightChild)
+
+#Time and Space complexity is O(N)
+def postOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    postOrderTraversal(rootNode.leftChild)
+    postOrderTraversal(rootNode.rightChild)
+    print(rootNode.data)
